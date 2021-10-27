89d65a748ed8749ab6d0df40a7deb96d5695c4cd
nduginets <nduginets@devexperts.com>
2021-10-27 22:42:26 +0300

    upd cnf

---
+++ run_classifier.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+isic19_root="/mnt/tank/scratch/nduginets"
+
+GPU=$1
+
+val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv"
+
+train_plain_old_clf="nfs/home/nduginets/gan-aug-analysis/splits/percentages/1_0"
+train_pix2pix_clf="1488"
+
+for split in $(seq 0 $SPLITS); do
+CUDA_VISIBLE_DEVICES=$GPU python3 train_comet_csv.py with \
+				train_root=${isic19_root} train_csv=${train_plain_old_clf}/train_${split}.csv epochs=100\
+				val_root=${isic19_root} val_csv=${val_csv} model_name="inceptionv4" exp_desc="Real" exp_name="gans.train_Real.inceptionv4.split${split}"
+done
