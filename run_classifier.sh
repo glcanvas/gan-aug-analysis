79d0a2d4f26cb3fb3bbf209b9fbe71e9e27adb65
nduginets <nduginets@devexperts.com>
2021-11-01 12:51:50 +0300

    upd pix2pix clf

--- run_classifier.sh
+++ run_classifier.sh
@@ -6,14 +6,15 @@ isic19_root="/mnt/tank/scratch/nduginets"
 val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv"
 
 train_plain_old_clf="/nfs/home/nduginets/gan-aug-analysis/splits/percentages/1_0"
-train_pix2pix_clf="1488"
 
-SPLITS=9
+DEVICES=$1
+SPLITS=$(echo $2 | tr ";" "\n")
 
-for split in $(seq 0 $SPLITS); do
-CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with \
+for split in $SPLITS; do
+CUDA_VISIBLE_DEVICES=$DEVICES python3 train_comet_csv.py with \
                                 train_root=${isic19_root} train_csv=${train_plain_old_clf}/train_${split}.csv epochs=100\
-                                val_root=${isic19_root} val_csv=${val_csv} model_name="inceptionv4" exp_desc="Real" exp_name="gans.train_Real.inceptionv4.split${split}"
+                                val_root=${isic19_root} val_csv=${val_csv} model_name="inceptionv4" exp_desc="Real"\
+                                exp_name="gans.train_Real.inceptionv4.split${split}"
 done
 
 # CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with train_root="/mnt/tank/scratch/nduginets" train_csv="/nfs/home/nduginets/gan-aug-analysis/splits/percentages/1_0/train_0.csv" epochs=100 val_root="/mnt/tank/scratch/nduginets" val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv" model_name="inceptionv4" exp_desc="Real" exp_name="gans.train_Real.inceptionv4.split0"
--- run_pix2pix_cl.sh
+++ run_pix2pix_cl.sh
@@ -7,9 +7,10 @@ val_csv="/nfs/home/nduginets/gan-aug-analysis/splits/isic2019-val.csv"
 
 train_pix2pix_clf="/nfs/home/nduginets/gan-aug-analysis/splits/different_gans/real-pix2pixhd/train.csv"
 
-SPLITS=9
+DEVICES=$1
+SPLITS=$(echo $2 | tr ";" "\n")
 
-for split in $(seq 0 $SPLITS); do
+for split in $SPLITS; do
 CUDA_VISIBLE_DEVICES=0 python3 train_comet_csv.py with \
                                 train_root=${isic19_root} train_csv=${train_pix2pix_clf} epochs=100\
                                 val_root=${isic19_root} val_csv=${val_csv} model_name="inceptionv4" exp_desc="pix2pix"\
